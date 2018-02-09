78303528ef8d6a55664fa146d752ed9f30705ac2
Alexis <Alexis@DESKTOP-TK1CAH7>
2018-02-09 15:06:36 +0100

    V1

---
+++ ProjetS4/src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+	public static void main(java.lang.String [] args) {
+		javax.swing.JOptionPane.showMessageDialog(null, "Aurevoir");
+		System.out.print("osef of the life");
+		
+	}
+}
