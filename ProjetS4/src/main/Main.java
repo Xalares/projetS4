9c9cc9af0c35a31a001dbb07e526ab47f7a5b700
Xalares <alexis_de-bacco@hotmail.fr>
2018-07-30 15:01:12 +0200

    Modif message

--- ProjetS4/src/main/Main.java
+++ ProjetS4/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	public static void main(java.lang.String [] args) {
 		javax.swing.JOptionPane.showMessageDialog(null, "Aurevoir");
-		System.out.print("test de modif");
+		System.out.print("test de modif encore");
 
 	}
 }
